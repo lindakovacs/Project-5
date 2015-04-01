a3e14224af5058a31c91ebf2d2cd9d52b1b89b6a
StijnVanDoorslaer <stijnvd_26@hotmail.com>
2015-04-01 15:12:19 +0200

    Startbestanden

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="description" content="Rent-A-Student">
+    <meta name="keywords" content="Rent,Student,Multimedia,Thomas More, Mechelen">
+    <meta name="author" content="Ande Timmerman,Manuel van den Notelaer,Nick van Puyvelde,Stijn Van Doorslaer">
+    
+    <link rel="icon" href="images/Rent-A-Student.png">
+    <title>Rent-A-Student</title>
+    
+    <!-- OPENGRAPH TAGS -->
+    <meta property="og:image" content="images/Rent-A-Student.png"/>
+    
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->    
+</head>
+<body>
+   
+    <!-- HEADER -->
+    <header>
+        <h1>Rent-A-Student</h1>
+    </header>
+    
+    <!-- NAV -->
+    <nav>
+        <ul>
+          <li><a href="">Pagina 1</a></li>
+          <li><a href="">Pagina 2</a></li>
+          <li><a href="">Pagina 3</a></li>
+        </ul>
+    </nav>
+    
+    <!-- SECTION -->
+    <section>
+        <h1>Inhoud</h1>
+        <p>Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw.</p>    
+    </section>
+    
+    <!-- FOOTER -->
+    <footer>
+        <p>&copy;2015</p>    
+    </footer>
+        
+</body>
+</html>
