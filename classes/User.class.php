eb7b77202ad3a2d7789b4b9f6234b00d3ee048f2
NickIMD <nickvanpuyvelde@MacBook-Pro-van-Nick.local>
2015-04-04 21:38:12 +0200

    Opslaan in database
    alles wordt opgeslagen in de database behalve foto is nog niet gelukt
    mss kan iemand anders daar naar kijken. En de foutmeldingen ga ik
    morgen doen maar op zich is da niet het zwaarste werk.

---
+++ classes/User.class.php
@@ -0,0 +1,132 @@
+<?php
+//include_once("Db.class.php");
+class User
+{
+    private $m_sFirstname;
+    private $m_sLastname;
+    private $m_sEmail;
+    private $m_sPassword;
+    private $m_iYear;
+    private $m_sEducation;
+    private $m_sCity;
+    private $m_sBio;
+    
+
+    public function __set($p_sProperty,$p_vValue)
+    {
+            switch($p_sProperty)
+            {
+                case 'Firstname';
+                $this->m_sFirstname = $p_vValue;
+                break;
+                
+                case 'Lastname';
+                $this->m_sLastname = $p_vValue;
+                break;
+                
+                case 'Email';
+                $this->m_sEmail = $p_vValue;
+                break;
+                
+                case 'Password';
+                $this->m_sPassword = $p_vValue;
+                break;
+                
+                case 'Year';
+                $this->m_iYear = $p_vValue;
+                break;
+                
+                case 'Education';
+                $this->m_sEducation = $p_vValue;
+                break;
+                
+                case 'City';
+                $this->m_sCity = $p_vValue;
+                break;
+                
+                case 'Bio';
+                $this->m_sBio = $p_vValue;
+                break;
+                
+            }
+    }
+
+    public function __get($p_sProperty)
+    {
+            switch($p_sProperty)
+            {
+                case 'Firstname';
+                return $this->m_sFirstname;
+                break;
+                
+                case 'Lastname';
+                return $this->m_sLastname;
+                break;
+                
+                case 'Email';
+                return $this->m_sEmail;
+                break;
+                
+                case 'Password';
+                return $this->m_sPassword;
+                break;
+                
+                case 'Year';
+                return $this->m_iYear;
+                break;
+                
+                case 'Education';
+                return $this->m_sEducation;
+                break;
+                
+                case 'City';
+                return $this->m_sCity;
+                break;
+                
+                case 'Bio';
+                return $this->m_sBio;
+                break;
+                
+                
+            }
+    }
+
+     public function save(){
+        $conn = new PDO('mysql:host=localhost;dbname=phpproject','root','root');
+        $statement = $conn->prepare('INSERT INTO gids
+                                                        (gids_voornaam,
+                                                        gids_naam,
+                                                        gids_email,
+                                                        gids_wachtwoord,
+                                                        gids_jaar,
+                                                        gids_richting,
+                                                        gids_stad,
+                                                        gids_bio        
+                                                         )
+                                     
+                                     VALUES(
+                                            :firstname,
+                                            :lastname,
+                                            :email,
+                                            :wachtwoord,
+                                            :jaar,
+                                            :richting,
+                                            :stad,
+                                            :bio
+                                            )'
+                                   ); 
+         
+         $statement->bindValue(':firstname',$this->Firstname);
+         $statement->bindValue(':lastname',$this->Lastname);
+         $statement->bindValue(':email',$this->Email);
+         $statement->bindValue(':wachtwoord',$this->Password);
+         $statement->bindValue(':jaar',$this->Year);
+         $statement->bindValue(':richting',$this->Education);
+         $statement->bindValue(':stad',$this->City);
+         $statement->bindValue(':bio',$this->Bio);
+         $statement->execute();
+         
+    }
+}
+?>
+
